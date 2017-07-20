19cff4e15adb8d75ebdfdf31c213211e626831e4
Jerome Wiley <jeromewiley@gmail.com>
2017-07-20 11:34:57 -0400

    removed fb instant module

--- web/modules/contrib/fb_instant_articles/src/MissingApiCredentialsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\fb_instant_articles;
-
-/**
- * API client is attempted to be used before it's been configured.
- */
-class MissingApiCredentialsException extends \Exception {
-}
